969ea92cde85ee774768e33b0582fd8cd772f046
agnesofdeo <agnesofdeo@gmail.com>
2023-02-07 18:04:07 +0900

    #1.4 Apollo Dev Tools

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const ALL_MOVIES = gql`
   query getMovies {
@@ -20,7 +21,9 @@ export default function Movies() {
   return (
     <ul>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/4{movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
